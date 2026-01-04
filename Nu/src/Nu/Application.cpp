6bf53776a6a77ff49ad130345a06838327598fab
ntroncoso05 <ntroncoso05@gmail.com>
2026-01-03 20:43:18 -0400

    Drawing a triangle for the first time and creating OpenGL Shaders

--- Nu/src/Nu/Application.cpp
+++ Nu/src/Nu/Application.cpp
@@ -23,6 +23,60 @@ namespace Nu {
 
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
+
+		// Vertex Array
+		glGenVertexArrays(1, &m_VertexArray);
+		glBindVertexArray(m_VertexArray);
+		// Vertex Buffer
+		glGenBuffers(1, &m_VertexBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+		float vertices[3 * 3] = {
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.0f,  0.5f, 0.0f
+		};
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0); // attribute a_Position;
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		// Index Buffer
+		glGenBuffers(1, &m_IndexBuffer);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+		unsigned int indices[3] = { 0, 1, 2 };
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			out vec3 v_Position; // variables that are transmitted between shaders are called (varying variables)
+			
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0f);
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color; // output color
+
+			in vec3 v_Position; // variables that are transmitted between shaders are called (varying variables)
+			
+			void main()
+			{
+				//color = vec4(0.8f, 0.2f, 0.3f, 1.0f);
+				color = vec4(v_Position * 0.5f + 0.5f, 1.0f);
+			}
+		)";
+
+		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
 	}
 
 	Application::~Application()
@@ -59,8 +113,12 @@ namespace Nu {
 	{
 		while (m_Running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			m_Shader->Bind();
+			//glBindVertexArray(m_VertexArray);
+			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
--- Nu/src/Platform/OpenGL/OpenGLContext.cpp
+++ Nu/src/Platform/OpenGL/OpenGLContext.cpp
@@ -17,6 +17,11 @@ namespace Nu {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		NU_CORE_ASSERT(status, "Failed to initialized Glad!");
+
+		NU_CORE_INFO("OpenGL Info:");
+		NU_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		NU_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		NU_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
